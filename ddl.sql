9fe676a71c94661b8ddc0e0054c4213e906df0c0
Negrey Sergey <snegprog@gmail.com>
2021-05-31 11:44:26 +0300

    ДОбавил комментарии к ddl и подправил README

--- ddl.sql
+++ ddl.sql
@@ -3,8 +3,8 @@ CREATE TABLE `tasks` (
   `name` varchar(512) NOT NULL DEFAULT '' COMMENT 'Название задачи',
   `description` varchar(2048) NOT NULL DEFAULT '' COMMENT 'Описание задачи',
   `status` int unsigned NOT NULL DEFAULT 0 COMMENT 'Статус исполнения задачи',
-  `create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `update_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP, -- Просто значение по умолчанию. Значение по умолчанию не желательно при кластере
+  `update_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,-- Просто значение по умолчанию. Значение по умолчанию не желательно при кластере
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
 
